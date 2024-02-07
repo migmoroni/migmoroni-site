6b78308329506398314ee08b749ad4af1a175e19
Miguel <miguelmcontato@gmail.com>
2024-02-07 17:39:47 -0300

    new features

---
+++ README.md
@@ -0,0 +1,10 @@
+# Meu Portfólio
+
+Bem-vindo ao meu portfólio no GitHub! Aqui você encontrará uma amostra do meu trabalho e projetos.
+
+Acesse o site aqui: [Link para o site portfolio](https://migmoroni.github.io/migmoroni-site/)
+
+## Contato
+
+Se você gostaria de entrar em contato comigo, sinta-se à vontade para me enviar um e-mail em miguelmcontato@gmail.com ou me encontrar no [LinkedIn](https://linkedin.com/seuusuario).
+
--- scripts/blog.js
+++ scripts/blog.js
@@ -3,25 +3,25 @@ async function getMediumPosts(username) {
     const parser = new RSSParser();
     const feed = await parser.parseURL(`https://medium.com/feed/${username}`);
     return feed.items;
-  }
+}
 
   // Função para exibir os posts na tela
-  async function showPosts(username) {
+async function showPosts(username) {
     const posts = await getMediumPosts(username);
     const postsContainer = document.getElementById('posts-container');
     
     posts.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.innerHTML = `
-        <h2>${post.title}</h2>
-        <p>${new Date(post.isoDate).toLocaleDateString()}</p>
-        <p>${post.contentSnippet}</p>
-        <a href="${post.link}" target="_blank">Read more</a>
-        <hr>
-      `;
-      postsContainer.appendChild(postElement);
+        const postElement = document.createElement('div');
+        postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${new Date(post.isoDate).toLocaleDateString()}</p>
+            <p>${post.contentSnippet}</p>
+            <a href="${post.link}" target="_blank">Read more</a>
+            <hr>
+        `;
+        postsContainer.appendChild(postElement);
     });
-  }
+}
 
-  // Chamada da função para exibir os posts (substitua 'your-username' pelo seu username do Medium)
-  showPosts('your-username');
+// Chamada da função para exibir os posts (substitua 'your-username' pelo seu username do Medium)
+showPosts('migmoroni');
